eb4dabc30852b538dbbd49de4b7f89c4d486ad11
vadim180569 <vadim9771505001@gmail.com>
2024-11-16 16:39:12 +0300

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,9 @@
+my_dict = {'Vadim': 1969 , 'Olga' : 1970}
+print(my_dict["Olga"])
+my_dict.pop('Olga')
+my_dict["Foma"] = 1900
+my_dict.update({"Anna" : 1993 , "Sergey" : 1991})
+print(my_dict)
+my_set = {1 , 2 , 3 , "bird" , 3 , "bird"}
+list_ = [1 , 2 , 3 , "bird" , 4 , 5]
+print(set(list_))
